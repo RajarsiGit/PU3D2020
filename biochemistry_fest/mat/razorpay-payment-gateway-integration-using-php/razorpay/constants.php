c3b4b0f37b47079ff97b6c3a8298939ea3a1fbca
Rajarsi Saha <rajarsi3997@gmail.com>
2020-08-09 11:24:17 +0530

    commit

--- biochemistry_fest/mat/razorpay-payment-gateway-integration-using-php/razorpay/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-// credential
-	define('RAZOR_KEY_ID', 'rzp_live_oeJxYE72LWd4FV');
-	define('RAZOR_KEY_SECRET', 'S4MZquthyunMfiW1voyTJAzM');
-
-?>
---
+++ razorpay/constants.php
@@ -0,0 +1,29 @@
+<?php
+	define('RAZOR_KEY_ID', 'rzp_live_9dsrCBHbWSIeO2');
+	define('RAZOR_KEY_SECRET', 'qXppLCEjOLqe4uMAIe92tpcX');
+
+	//define('RAZOR_KEY_ID', 'rzp_test_6lTpg1E0LVrFPS');
+	//define('RAZOR_KEY_SECRET', 'ecVekFi15HTmune4wWvD0Atz');	
+
+	$amount = $_POST['q49_calculation'];
+	$name = $_POST['q8_fullName8'];
+	$email = $_POST['q38_email38'];
+	$qualification = $_POST['q43_qualification'];
+	$designation = $_POST['q48_designation'];
+	$add = $_POST['q34_institutionName'];	
+	$phone = $_POST['q12_phoneNumber12'];
+	$intend = $_POST['q46_iIntend'];
+	$accom = $_POST['q47_doYou'];
+	$calc = $_POST['q49_calculation'];
+	//$fee = 1.02;
+	//$calc = $calc * $fee;
+	$unique_id = $_POST['unique_id'];
+
+	$name_str = implode(" ", $name);
+	$add_str = implode(", ", $add);
+	$phone_str = implode(" ", $phone);
+	$intend_str = implode(", ", $intend);
+	$amount .= "00";
+
+	$phone_str = preg_replace('/  */', '', $phone_str);
+?>
